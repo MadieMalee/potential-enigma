1846c62d8594c49a5735424c485059d71e29d9d8
Madison <madie-v@live.com>
2023-11-08 17:22:24 -0600

    made an array of questions for readme and created the generateMarkdown function'

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('../Develop/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please add a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'screenshot',
+    message: 'Please provide the path to the image you want to use as the screenshot.',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and/or examples of what you might us this for.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', "Boost1.0", 'None'],
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: 'Please add your project URL so any user can access your deployed application.',
+  },
+  {
+    type: 'input',
+    name: 'features',
+    message: 'Please list any cool features your project might have here.',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Please list any contributors by using their github username:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
+
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'none') {
+    // Replace 'License' with the actual license type you support
+    return `![Github license](https://img.shields.io/badge/License-${license}-brightgreen)`;
+  }
+  return '';
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +20,44 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ## Table of Contents
+  - [Description](#description)
+  - [Webpage Output](#screenshot)
+  - [Deployed Application URL](#link)
+  - [Features](#features)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Questions](#questions)
+
+  ## Description
+  ${data.description}
+  
+  ## Webpage Output
+  ![alt-text](${data.screenshot})
+
+  ## Deployed Application URL 
+  ${data.link}
+  
+  ## Features 
+  ${data.features}
+
+  ## How to use this Application:
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license. ${renderLicenseLink(data.license)}
+  
+  ## Contributors
+  ${data.contribution}
+
+  ## Questions
+  GitHub: [${data.github}](https://github.com/${data.github})  
+  For any questions, please contact me at ${data.email}.
+
+`
 }
 
 module.exports = generateMarkdown;
